8fa60650890b1cbfd483b11caf7bd815ac94f44f
ramiroandrade <ramiroandrade@users.noreply.github.com>
2020-02-18 22:01:55 +0100

    cambios 20200218

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(function() {
   
   $(".menu").click(function(e) {
-    $(".menu__content").toggleClass("hide");
-    $("body").toggleClass("no-scroll");
+    // $(".menu__content").toggleClass("hide");
+    $("body").toggleClass("menu-open");
   });
 	
 });
