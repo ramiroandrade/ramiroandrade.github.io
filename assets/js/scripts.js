96451d9a08900416585a365a8219b32ece9f5624
ramiroandrade <ramiroandrade@users.noreply.github.com>
2020-02-27 21:28:31 +0100

    Agregada la animación a los elementos y un fix para el overflow hidden del body

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,19 @@
 $(function() {
+  AOS.init();
   
   $(".menu").click(function(e) {
     // $(".menu__content").toggleClass("hide");
     $("body").toggleClass("menu-open");
+    
+    if ($("body").hasClass("no-scroll")) {
+      $("body").removeClass("no-scroll");
+    }
+
+    setTimeout(function() {
+      if ($("body").hasClass("menu-open")) {
+        $("body").addClass("no-scroll");
+      }
+    }, 400);
   });
 	
 });
