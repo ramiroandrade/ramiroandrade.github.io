9441799223633b37a752368002665ff2c839e6da
Ramiro Andrade <60939924+ramiroandrade@users.noreply.github.com>
2023-05-01 22:12:37 +0200

    Version Light 2023

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,7 +24,7 @@ $(function() {
 
 function myFunction() {
   var element = document.body;
-  element.classList.toggle("light-mode");
+  element.classList.toggle("dark-mode");
 }
 
 (function (factory) {
